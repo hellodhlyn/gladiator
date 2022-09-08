1e051b35cfa0c470a060e207c2c50251c3315f94
Hoerin Doh <hellodhlyn@gmail.com>
2022-09-08 20:02:09 +0900

    [email-route-generator] add an ip-based rate limiter

--- email-route-generator/src/index.ts
+++ email-route-generator/src/index.ts
@@ -1,4 +1,7 @@
 export interface Env {
+	// KVs
+	RATE_LIMITS: KVNamespace;
+
 	// Secrets
   CF_ACCOUNT_ID: string;
 	CF_ZONE_ID: string;
@@ -9,10 +12,13 @@ export interface Env {
 
 const CF_API_HOST = "https://api.cloudflare.com";
 const EMAIL_EXPIRE_HOURS = 24;
+const RATE_LIMIT_HOURS = 24;
+const RATE_LIMIT_COUNT = 1;
+const DURATION_HOUR = 60 * 60 * 1000;
 
 async function generateAddress(request: Request, env: Env): Promise<Response> {
 	const address = `contact_${Math.random().toString(36).slice(-6)}@lynlab.co.kr`;
-	const expireAt = new Date((new Date()).getTime() + EMAIL_EXPIRE_HOURS * 60 * 60 * 1000);
+	const expireAt = new Date((new Date()).getTime() + EMAIL_EXPIRE_HOURS * DURATION_HOUR);
 	const generationInfo = {
 		ip: request.headers.get("CF-Connecting-IP"),
 		exp: expireAt.getTime(),
@@ -48,11 +54,27 @@ async function generateAddress(request: Request, env: Env): Promise<Response> {
 	});
 }
 
+async function rateLimit(request: Request, env: Env): Promise<boolean> {
+	const ip = request.headers.get("CF-Connecting-IP");
+	const rateLimitKey = `email-route-generator:ip:${ip}`;
+	const requests = (JSON.parse((await env.RATE_LIMITS.get(rateLimitKey)) || "[]") as number[])
+		.filter((timestamp) => timestamp > (new Date().getTime() - RATE_LIMIT_HOURS * DURATION_HOUR));
+	if (requests.length >= RATE_LIMIT_COUNT) {
+		return false;
+	}
+
+	await env.RATE_LIMITS.put(rateLimitKey, JSON.stringify([...requests, new Date().getTime()]));
+	return true;
+}
+
 export default {
 	async fetch(request: Request, env: Env, ctx: ExecutionContext): Promise<Response> {
 		const { pathname } = new URL(request.url);
 		if (request.method !== "POST" || pathname !== "/generate") {
 			return new Response("not found", { status: 404 });
+		}
+		if (!await rateLimit(request, env)) {
+			return new Response("too many requests", { status: 429 });
 		}
 
 		return generateAddress(request, env);
